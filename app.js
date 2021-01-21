70cf10f2825d1c75c63aa5b038035516bfb5e173
Tomolo997 <tomazovsenjak7@gmail.com>
2021-01-21 08:20:29 +0100

    added the navigation to the left side

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Gen random data
-const N = 300;
+const N = 3000;
 const gData = [...Array(N).keys()].map(() => ({
   lat: (Math.random() - 0.5) * 180,
   lng: (Math.random() - 0.5) * 360,
@@ -7,8 +7,11 @@ const gData = [...Array(N).keys()].map(() => ({
   color: ['red', 'white', 'blue', 'green'][Math.round(Math.random() * 3)],
 }));
 
+console.log(gData);
 Globe()
   .globeImageUrl('//unpkg.com/three-globe/example/img/earth-day.jpg')
   .pointsData(gData)
   .pointAltitude('size')
   .pointColor('color')(document.getElementById('globeViz'));
+
+//get earthquake data
