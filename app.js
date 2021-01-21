c83829099a3e381102a340df9cf036ab40b3f8d1
Tomolo997 <tomazovsenjak7@gmail.com>
2021-01-21 07:57:46 +0100

    got the globe working

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
-function initialize() {
-  var earth = new WE.map('earth_div');
-  WE.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '© OpenStreetMap contributors',
-  }).addTo(earth);
-}
+// Gen random data
+const N = 300;
+const gData = [...Array(N).keys()].map(() => ({
+  lat: (Math.random() - 0.5) * 180,
+  lng: (Math.random() - 0.5) * 360,
+  size: Math.random() / 3,
+  color: ['red', 'white', 'blue', 'green'][Math.round(Math.random() * 3)],
+}));
+
+Globe()
+  .globeImageUrl('//unpkg.com/three-globe/example/img/earth-day.jpg')
+  .pointsData(gData)
+  .pointAltitude('size')
+  .pointColor('color')(document.getElementById('globeViz'));
